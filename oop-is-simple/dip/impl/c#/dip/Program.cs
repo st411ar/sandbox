46103c812d549a0d9ad2a2443d1df9c3a5d1bdf9
Vitaly Kiselev <st411ar@ya.ru>
2019-12-05 22:17:59 +0300

    added abstract demonstration

--- oop-is-simple/dip/impl/c#/dip/Program.cs
+++ oop-is-simple/dip/impl/c#/dip/Program.cs
@@ -2,19 +2,40 @@
 
 namespace DependencyInversionPrinciple
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        	DemonstrateInterface();
-        }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DemonstrateInterface();
+			DemonstrateAbstract();
+		}
 
 
-        private static void DemonstrateInterface()
-        {
-            var musicalInstrument = Friend.GiveInstrument();
+		private static void DemonstrateInterface()
+		{
+			Console.WriteLine("Interface demonstration start");
 
-            musicalInstrument.GetSound();
-        }
-    }
+			var musicalInstrument = Friend.GiveInstrument();
+
+			musicalInstrument.GetSound();
+
+			Console.WriteLine("Interface demonstration stop");
+		}
+
+        private static void DemonstrateAbstract()
+		{
+			Console.WriteLine("Abstract demonstration start");
+
+			var firmName = "Marys";
+			var instrumentName = "Harmonica";
+
+			var pawnshop = new Pawnshop();
+
+			var musicalInstrument = pawnshop.PrepareToSell(firmName, instrumentName);
+
+			musicalInstrument.GetSound();
+
+			Console.WriteLine("Abstract demonstration stop");
+		}
+	}
 }
