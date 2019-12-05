37a6777dcc8da76c3dafcf584ff59015cb7bf76c
Vitaly Kiselev <st411ar@ya.ru>
2019-12-05 21:30:51 +0300

    created abstract instrument

---
+++ oop-is-simple/dip/impl/c#/dip/AInstrument.cs
@@ -0,0 +1,9 @@
+namespace DependencyInversionPrinciple
+{
+	abstract class AInstrument
+	{
+		public abstract void GetSound();
+		public abstract void Repair();
+		public abstract void Pack();
+	}
+}
